6b60f2d7fb320b09a1a303c83466556ebd84971d
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-10 15:28:58 +0300

    (6) changed whitespaces

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
-"""
-module for the sum_mixed_list function with type annotation
-"""
+
+""" module for the sum_mixed_list function with type annotation """
 import typing
 
 
 def sum_mixed_list(mxd_lst: typing.List[typing.Union[float | int]]) -> float:
-    """
-    returns the sum of a list of floats and integers only.
-    """
+    """ returns the sum of a list of floats and integers only. """
     return float(sum(mxd_lst))
