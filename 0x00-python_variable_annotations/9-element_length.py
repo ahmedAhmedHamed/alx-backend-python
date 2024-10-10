4e53d82edb18eaddc7375ce85bbeabcf1375103e
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-10 15:48:30 +0300

    (9) fixed annotation

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,9 +2,9 @@
 """
 module for the make_multiplier function with type annotation
 """
-from typing import Callable, Tuple, List, Sequence
+from typing import Callable, Tuple, List, Sequence, Iterable
 
 
-def element_length(lst: Sequence) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """ a tuple of number, number """
     return [(i, len(i)) for i in lst]
