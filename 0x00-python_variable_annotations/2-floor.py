9618ea5ea9a8718bbab6050a2b7918370483af01
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-10 14:59:55 +0300

    (2)

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+module for the floor function with type annotation
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    returns the floor of n
+    """
+    return math.floor(n)
