efec285ec11416fb74427ad39d78c584965a62c3
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-10 15:11:46 +0300

    (5) fixed docstring

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -7,6 +7,6 @@ from typing import List
 
 def sum_list(input_list: List[float]) -> float:
     """
-    adds a and b, meant for floats only.
+    returns the sum of a list of floats.
     """
     return sum(input_list)
