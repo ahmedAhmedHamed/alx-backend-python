ebeb6ee115a605e6c87ad704248ec7f7782a7c53
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-10 15:32:40 +0300

    (6) fixed Union with a ','

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-""" mixed list question """
+""" module for the sum_mixed_list function with type annotation """
 from typing import Union, List
 
 
-def sum_mixed_list(mxd_lst: List[Union[float | int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
     """ returns sum as a float. """
     return float(sum(mxd_lst))
