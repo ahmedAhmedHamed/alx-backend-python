10f045e51f9e0394c00dd8f24e5c5e4fa3dad1cd
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-10 14:55:43 +0300

    (0)

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+module for the add function with type annotation
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    adds a and b, meant for floats only.
+    """
+    return a + b
