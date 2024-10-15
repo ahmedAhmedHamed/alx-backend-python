a808715830c7aba8f6d8f1d48775b16ddbf8067a
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-15 12:07:36 +0300

    (0) type annotations generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """ async generator tutorial """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator() -> float:
+async def async_generator() -> Generator[float]:
     """ yields a random number between 0 and 10 every second for 10 seconds """
     for i in range(10):
         await asyncio.sleep(1)
