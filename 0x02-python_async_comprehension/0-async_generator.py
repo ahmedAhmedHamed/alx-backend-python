271e1f0d335489c52f699f0254c4b6877de33cde
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-15 12:12:15 +0300

    (0) using AsyncGenerator typehint

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """ async generator tutorial """
 import asyncio
 import random
-from typing import Generator
+from typing import AsyncGenerator
 
 
-async def async_generator() -> Generator[float]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """ yields a random number between 0 and 10 every second for 10 seconds """
     for i in range(10):
         await asyncio.sleep(1)
