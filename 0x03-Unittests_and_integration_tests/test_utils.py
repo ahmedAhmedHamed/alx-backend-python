674c9d7cee7ebbd7c645ea4037181e89de27de45
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-31 09:56:12 +0300

    (1)

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -22,3 +22,13 @@ class TestAccessNestedMap(TestCase):
         this test is ran 3 times for different inputs
         """
         self.assertEqual(utils.access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """
+        tries to access non existing keys and raises key error
+        """
+        self.assertRaises(KeyError, utils.access_nested_map, nested_map, path)
