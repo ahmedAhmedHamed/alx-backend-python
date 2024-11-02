84f1938a76c07708bda9254e64d4001a2c12f2f1
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-11-02 19:13:36 +0200

    (5)

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,8 @@
 tests the client module
 """
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest import mock
+from unittest.mock import patch, MagicMock, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -14,8 +15,8 @@ class TestGithubOrgClient(unittest.TestCase):
     """
 
     @parameterized.expand([
-        ("google", ),
-        ("abc", )
+        ("google",),
+        ("abc",)
     ])
     @patch('client.get_json')
     def test_org(self, org: str, mock_get_json: MagicMock):
@@ -25,3 +26,11 @@ class TestGithubOrgClient(unittest.TestCase):
         my_client.org()
         mock_get_json.assert_called_once_with(
             f"https://api.github.com/orgs/{org}")
+
+    def test_public_repos_url(self):
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mock_org:
+            payload = "returned payload"
+            mock_org.return_value = {"repos_url": "returned payload"}
+            my_client = GithubOrgClient("test")
+            self.assertEqual(my_client._public_repos_url, payload)
