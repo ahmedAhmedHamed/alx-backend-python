fa51eb15ab0e3a1bb2b630e90219ec3ae2029ab8
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-11-02 18:09:03 +0200

    (4)

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+tests the client module
+"""
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """
+    tests access nested map
+    """
+
+    @parameterized.expand([
+        ("google", "https://api.github.com/orgs/google"),
+        ("abc", "https://api.github.com/orgs/abc")
+    ])
+    @patch('client.get_json')
+    def test_org(self, org, expected, mock_get_json):
+        """ test that GithubOrgClient.org returns the correct value. """
+        mock_get_json.return_value = expected
+        my_client = GithubOrgClient(org)
+        self.assertEqual(expected, my_client.org)
+        self.assertEqual(expected, my_client.org)
+        mock_get_json.assert_called_once_with(expected)
