e84b924bbd7dd0a90f71caeb4428be3250fd11a8
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2024-11-04 21:38:01 +0200

    (9) back to XLICENSE

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -87,7 +87,14 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         self.assertEqual(my_client.org, self.org_payload)
         self.assertEqual(my_client.repos_payload, self.repos_payload)
         self.assertEqual(my_client.public_repos(), self.expected_repos)
-        self.assertEqual(my_client.public_repos("amongus"), [])
+        self.assertEqual(my_client.public_repos("XLICENSE"), [])
+        self.mock.assert_called()
+
+    def test_public_repos_with_license(self):
+        """ tests public repos using apace-2.0 license """
+        my_client = GithubOrgClient("google")
+        self.assertEqual(my_client.public_repos(), self.expected_repos)
+        self.assertEqual(my_client.public_repos("XLICENSE"), [])
         self.assertEqual(my_client.public_repos(
             "apache-2.0"), self.apache2_repos)
         self.mock.assert_called()
